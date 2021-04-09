9b11d8fac41535e602a79c43b2afa0f0ec1f38f8
Adam k <44911605+adamkatt@users.noreply.github.com>
2021-04-09 19:18:16 -0400

    routes update
    added new routes, and fixed existing ones

--- routes/recipes.js
+++ routes/recipes.js
@@ -39,23 +39,29 @@ router.post('/',  async (req, res, next) => {
   }
 })
 
-/*
+
 // Getting One
 router.get('/:id', getrecipe, (req, res) => {
-  res.json(res.recipe)
-})*/
+  res.json(res.arecipe)
+})
 
-/*
+
 // Updating One
 router.patch('/:id', getrecipe, async (req, res) => {
   if (req.body.name != null) {
-    res.recipe.name = req.body.name
+    res.arecipe.name = req.body.name
   }
-  if (req.body.subscribedToChannel != null) {
-    res.recipe.subscribedToChannel = req.body.subscribedToChannel
+  if (req.body.ingredients != null) {
+    res.arecipe.ingredients = req.body.ingredients
+  }
+  if (req.body.instructions != null) {
+    res.arecipe.instructions = req.body.instructions
+  }
+    if (req.body.author != null) {
+    res.arecipe.author = req.body.author
   }
   try {
-    const updatedrecipe = await res.recipe.save()
+    const updatedrecipe = await res.arecipe.save()
     res.json(updatedrecipe)
   } catch (err) {
     res.status(400).json({ message: err.message })
@@ -65,26 +71,29 @@ router.patch('/:id', getrecipe, async (req, res) => {
 // Deleting One
 router.delete('/:id', getrecipe, async (req, res) => {
   try {
-    await res.recipe.remove()
+    await res.arecipe.remove()
     res.json({ message: 'Deleted recipe' })
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
 })
-*/
+
+
 async function getrecipe(req, res, next) {
-  let recipe
+  let arecipe
   try {
-    recipe = await recipe.findById(req.params.id)
-    if (recipe == null) {
+    arecipe = await recipe.findById(req.params.id)
+    if (arecipe == null) {
       return res.status(404).json({ message: 'Cannot find recipe' })
     }
   } catch (err) {
     return res.status(500).json({ message: err.message })
   }
 
-  res.recipe = recipe
+  res.arecipe = arecipe
   next()
 }
+
+
 
 module.exports = router
